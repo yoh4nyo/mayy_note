016bbd230d5183b41d6f0db52a71a7438ffde9b1
yohan <baiiky77185@gmail.com>
2024-06-02 17:39:46 +0200

    update

---
+++ html/menu_prof.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <link rel="stylesheet" href="../css/style_menu_prof.css" />
+    <title>Document</title>
+  </head>
+  <body>
+
+    <div class="off-screen-menu">
+      <ul>
+        <li><i class="fa-solid fa-check fa-border fa-lg"></i><a href="#">Gestion des notes</a></li> <br>
+        <li><i class="fa-solid fa-list fa-border fa-lg"></i><a href="#">Consulter</a></li> <br>
+        <li class="disconnect"><i class="fa-solid fa-power-off fa-border fa-lg"></i><a href="#">Déconnexion</a></li>
+      </ul>
+    </div>
+
+    <nav>
+      <div class="ham-menu">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </nav>
+
+    <script src="../js/scripts.js"></script>
+  </body>
+</html>
