5935f8a2844eef0d7ba50eec454df83a969ffade
yohan <baiiky77185@gmail.com>
2024-06-02 19:07:27 +0200

    update etudiant

---
+++ html/etudiant_dashboard.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mayy Notes - Statistique</title>
+    <link rel="stylesheet" href="../css/style_etudiant_dashboard.css">
+</head>
+<body>
+    <div class="main">
+        <div>
+            <h1>Statistique | Nom Prénom TPE TD3</h1>
+        </div>
+        <div class="stats">
+            <div class="stat-box">
+                <h2>rang sur la promo:</h2>
+                <p>30/80</p>
+            </div>
+            <div class="stat-box">
+                <h2>moyenne générale de la promo:</h2>
+                <p>10,08</p>
+            </div>
+            <div class="stat-box">
+                <h2>moyenne générale:</h2>
+                <p>12,3</p>
+            </div>
+        </div>
+        <div class="content">
+            <div class="chart">
+                <h3>Vos moyennes:</h3>
+                <img src="https://www.lecfomasque.com/wp-content/uploads/2014/03/moyenne-sur-graphique-3.jpg" alt="Histogramme des notes">
+            </div>
+            <div class="right-content">
+                <div class="appreciation">
+                    <h3>Appréciation:</h3>
+                    <p>excellent travail</p>
+                </div>
+                <div class="validation">
+                    <h3>Validation du semestre:</h3>
+                    <p>OUI</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
